69c10f66e3e4282c23957943fed911775a465e1d
feedop <110346745+feedop@users.noreply.github.com>
2022-11-20 12:53:21 +0100

    Implemented radix sort

--- kernel.cu
+++ kernel.cu
@@ -1,35 +1,121 @@
 ﻿
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "curand_kernel.h"
 
-#include <stdio.h>
+#include "radix_sort.cuh"
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+#include <iostream>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+cudaError_t generateRandoms(unsigned int* dev_input, const int& sequenceLength, const int& numberOfSequences);
+
+// prints a few sequences from the beginning 
+void head(unsigned int* input, float milliseconds, const int& sequenceLength, const int& numberOfSequences)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+    int vecLen = sequenceLength > 3 ? 3 : sequenceLength;
+    int vectors = numberOfSequences > 10 ? 10 : numberOfSequences;
+    std::vector<std::bitset<32>>bitset(vecLen * vectors);
+    for (int i = 0; i < vecLen; i++)
+    {
+        for (int j = 0; j < vectors; j++)
+        {
+            bitset[i * vectors + j] = std::bitset<32>(input[(i + sequenceLength - vecLen) * numberOfSequences + j]);
+        }
+    }
+
+    std::cout << "Values: \n";
+    for (int i = 0; i < vectors; i++)
+    {
+        for (int j = vecLen - 1; j >= 0; j--)
+        {
+            std::cout << bitset[j * vectors + i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "Time elapsed: " << milliseconds / 1000 << " seconds\n";
+}
+
+__global__ void generateRandomsKernel(unsigned int *input, const int sequenceLength, const int numberOfSequences)
+{
+    unsigned int seed = blockIdx.x * blockDim.x + threadIdx.x;
+    if (seed >= sequenceLength * numberOfSequences) return;
+    curandState localState;
+    curand_init(seed, 0, 0, &localState);
+    input[seed] = curand(&localState);
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+    cudaError_t cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    int sequenceLength = 32;
+    int numberOfSequences = 100000;    
+
+    unsigned int* input = (unsigned int*)malloc(sizeof(int) * sequenceLength * numberOfSequences);
+    unsigned int* dev_input = 0;
+
+    cudaStatus = cudaMalloc((void**)&dev_input, sizeof(int) * sequenceLength * numberOfSequences);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // generate random sequences
+    cudaEventRecord(start);
+    cudaStatus = generateRandoms(dev_input, sequenceLength, numberOfSequences);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "addWithCuda failed!");
         return 1;
     }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(input, dev_input, sizeof(int) * sequenceLength * numberOfSequences, cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
 
-    printf("\n%d", __cplusplus);
+    // print
+    head(input, milliseconds, sequenceLength, numberOfSequences);
+
+    // ---------------------------------------------------------------------------------
+
+    // radix sort input
+    cudaEventRecord(start);
+    cudaStatus = radixSort(&dev_input, sequenceLength, numberOfSequences);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(input, dev_input, sizeof(int) * sequenceLength * numberOfSequences, cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // print
+    head(input, milliseconds, sequenceLength, numberOfSequences);
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -38,86 +124,26 @@ int main()
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
     }
-
+Error:
+    free(input);
+    cudaFree(dev_input);
+    //getchar();
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+// fills the input array with random unsigned ints
+cudaError_t generateRandoms(unsigned int* dev_input, const int& sequenceLength, const int& numberOfSequences)
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
     cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+    int threadsPerBlock = sequenceLength * numberOfSequences < 1024 ? sequenceLength * numberOfSequences : 1024;
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+    generateRandomsKernel << <(sequenceLength * numberOfSequences + threadsPerBlock - 1) / threadsPerBlock, threadsPerBlock >> > (dev_input, sequenceLength, numberOfSequences);
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching generateRandomsKernel!\n", cudaStatus);
     }
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
     return cudaStatus;
 }
